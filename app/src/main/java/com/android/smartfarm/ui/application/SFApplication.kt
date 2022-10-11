7a0d5a07cfe44744b50738e7bd307a1d3f85e5ac
jino <whangwan@gmail.com>
2022-10-11 21:54:14 +0900

    Hilt 사용을 위한 Application 클래스 추가 및 manifest에 등록 기타 색상과 이미지,seekbar의 custom thumb추가

---
+++ app/src/main/java/com/android/smartfarm/ui/application/SFApplication.kt
@@ -0,0 +1,8 @@
+package com.android.smartfarm.ui.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SFApplication : Application() {
+}
